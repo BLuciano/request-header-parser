13c48d28ef00f85b8690affd3193f0cdf35902ff
Luciano <lucianobruzzoni@yahoo.com>
2016-02-24 05:43:55 +0000

    Displaying header data without any constrains

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
+var ip, lang, os;
 
 app.get('/', function(req, res) {
- 
+  var headerData = req.headers;
+  ip = headerData['x-forwarded-for'];
+  lang = headerData['accept-language'];
+  os = headerData['user-agent'];
+
+  res.send({
+    "ipaddress": ip,
+    "language": lang,
+    "software": os
+  });
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
